26fe6a0448f8b1eb4b92ea2ae93877e715a8b4c0
Dmytro-Leonov <leonovdmitriy02@gmail.com>
2020-11-07 21:49:14 +0200

    4pz done

--- app.js
+++ app.js
@@ -6,55 +6,46 @@ let index = 0
 let delay = 15000
 let nOfSlides = document.getElementsByClassName("slider_element").length
 
-slider.style.width = (nOfSlides)*100 + '%'
+slider.style.width = (nOfSlides) * 100 + '%'
 
-function slide(){
-    if(index < nOfSlides - 1)
-    {
+function slide() {
+    if (index < nOfSlides - 1) {
         index += 1
         slider.style.transition = '0.3s'
-        slider.style.transform = 'translate(' + (index) * (-100/nOfSlides) + '%)'
+        slider.style.transform = 'translate(' + (index) * (-100 / nOfSlides) + '%)'
     }
-    else
-    {
+    else {
         index = 0
-        slider.style.transform = 'translate(' + (index) * (-100/nOfSlides) + '%)'
+        slider.style.transform = 'translate(' + (index) * (-100 / nOfSlides) + '%)'
         slider.style.transition = '1s'
     }
 }
-let timer = setInterval( slide, delay);
+let timer = setInterval(slide, delay);
 
 rightArrow.addEventListener('click', slide)
 
 leftArrow.addEventListener('click', () => {
-    if(index > 0)
-    {
+    if (index > 0) {
         index -= 1
         slider.style.transition = '0.3s'
-        slider.style.transform = 'translate(' + (index) * (-100/nOfSlides) + '%)'
+        slider.style.transform = 'translate(' + (index) * (-100 / nOfSlides) + '%)'
     }
-    else
-    {
-        index = nOfSlides-1
-        slider.style.transform = 'translate(' + (index) * (-100/nOfSlides) + '%)'
+    else {
+        index = nOfSlides - 1
+        slider.style.transform = 'translate(' + (index) * (-100 / nOfSlides) + '%)'
         slider.style.transition = '1s'
     }
 })
 
 document.querySelectorAll('.collapse_button').forEach(button => {
-    button.addEventListener('click', ()=>{
+    button.addEventListener('click', () => {
         let collapse_content = button.nextElementSibling
         button.classList.toggle('collapse_button-active')
-        if(button.classList.contains('collapse_button-active'))
-        {
+        if (button.classList.contains('collapse_button-active')) {
             collapse_content.style.maxHeight = collapse_content.scrollHeight + 'px'
         }
-        else
-        {
+        else {
             collapse_content.style.maxHeight = null
         }
     })
 })
-
-
-    
