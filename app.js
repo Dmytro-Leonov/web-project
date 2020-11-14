d691f151a0382acaf995dd3f3b39e716ac478877
Dmytro-Leonov <leonovdmitriy02@gmail.com>
2020-11-14 22:41:44 +0200

    pz5 done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//slider
 const slider = document.querySelector('.slider')
 const leftArrow = document.querySelector('.left')
 const rightArrow = document.querySelector('.right')
@@ -37,6 +38,7 @@ leftArrow.addEventListener('click', () => {
     }
 })
 
+//collapse content
 document.querySelectorAll('.collapse_button').forEach(button => {
     button.addEventListener('click', () => {
         let collapse_content = button.nextElementSibling
@@ -49,3 +51,116 @@ document.querySelectorAll('.collapse_button').forEach(button => {
         }
     })
 })
+
+//animations
+window.addEventListener('load', () => {
+    let blocks = document.querySelectorAll('.aboutUsBlock')
+    blocks.forEach(block => {
+            block.classList.add('aboutUsBlock_appear')
+        })
+})
+
+window.addEventListener('scroll', () => {
+    let blocks = document.querySelectorAll('.product')
+    blocks.forEach(block => {
+        let blockPos = block.getBoundingClientRect().top
+        let screenPos = window.innerHeight
+        if(blockPos < screenPos)
+            block.classList.add('product_appear')
+      })
+})
+
+//popup
+let openButton = document.querySelector('.privacy_policy')
+let closeButton = document.querySelector('.close_button')
+let overlay = document.getElementById('overlay')
+
+openButton.addEventListener('click', () => {
+    document.querySelector('.modal').classList.add('active')
+    overlay.classList.add('active')
+  })
+
+closeButton.addEventListener('click', () => {
+    document.querySelector('.modal').classList.remove('active')
+    overlay.classList.remove('active')
+})  
+
+//form validation
+const form = document.getElementById('form');
+const surname = document.getElementById('surname')
+const name_ = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	checkInputs()
+});
+
+function checkInputs() {
+	const surnameValue = surname.value.trim()
+	const nameValue = name_.value.trim()
+	const emailValue = email.value.trim()
+	const passwordValue = password.value
+    const password2Value = password2.value
+    let lettersOnly = /^[A-Za-z]+$/
+	
+	if(surnameValue === '') {
+		setErrorFor(surname, 'Surname cannot be blank')
+	} else if(!surnameValue.match(lettersOnly)) {
+        setErrorFor(surname, 'Invalid characters in surname')
+    } else {
+		setSuccessFor(surname)
+    }
+    
+    if(nameValue === '') {
+		setErrorFor(name_, 'Name cannot be blank')
+	} else if(!nameValue.match(lettersOnly)) {
+        setErrorFor(name_, 'Invalid characters in name')
+    } else {
+		setSuccessFor(name_);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank')
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Invalid email')
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank')
+	} else if(passwordValue.length < 8) {
+		setErrorFor(password, 'Password must be at least 8 characters long')
+    } else if(passwordValue.length > 20) {
+        setErrorFor(password, 'Password is too long')
+    } else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords do not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
